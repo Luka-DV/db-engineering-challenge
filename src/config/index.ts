0a203cfe8bb352a67c48473b0daa3d40aa5c1cd7
luka-dv <luka.guva@gmail.com>
2026-04-21 14:15:50 +0200

    refactor dry_run and add log_level

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,3 @@
-/* 
-DBOX_API_KEY=your_databox_api_key
-
-# Used only for one-time data source/dataset creation via curl:
-# DBOX_ACCOUNT_ID=123456
-# DBOX_DATA_SOURCE_ID=1234567
-
-DBOX_GITHUB_DATASET_ID=your_gh_dataset_id
-DBOX_NASA_DATASET_ID=your_nasa_dataset_id
-
-GH_ACCESS_TOKEN=your_gh_personal_access_token
-GH_REPOS=microsoft/TypeScript,axios/axios,nodejs/node
-
-NASA_API_KEY=your_nasa_api_key
-*/
-
 import { z } from "zod";
 
 
@@ -25,7 +9,7 @@ const ghRepoSchema = z.string()
                 code: "custom",
                 message: `Invalid owner/repo format: ${ownerRepoString}`,
                 input: ownerRepoString,
-            });
+            })
             return z.NEVER;
         };
         
@@ -47,17 +31,22 @@ const envDataSchema = z.object({
             .map(ghPair => ghPair.trim())
             .filter(Boolean)
         })
-        .pipe(z.array(ghRepoSchema)),
+        .pipe(z.array(ghRepoSchema).min(1, "GH_REPOS must contain at least one valid owner/repo")),
 
     NASA_API_KEY: z.string().trim().min(1),
 
-    DRY_RUN: z.string()
+    /* DRY_RUN: z.string()
         .trim()
         .toLowerCase()
         .transform(string => {
             if(string === "false") return false;
             else return true;
-        }).default(true)
+        }).default(true) */
+
+    DRY_RUN: z.stringbool().default(true), // Zod v4
+                                                       
+    LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace"])
+                   .default("info"),
 });
 
 
