13aac5700cbeef46aae2940e99df905c02880146
luka-dv <luka.guva@gmail.com>
2026-04-21 14:18:24 +0200

    import log level, refactor

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,39 +8,55 @@ Include basic local logging of:
  */
 
 import pino from "pino";
+import { config } from "../config/index.js";
+
+
+// Define the transport configuration only when the output stream is connected to a terminal (TTY)
+const transport = process.stdout.isTTY ?
+    {transport: 
+            {
+                target: "pino-pretty",
+                options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: "pid,hostname"
+                }
+            }
+    } : {};
 
 export const logger = pino({
-    level: "info",
+    level: config.LOG_LEVEL,
     timestamp: pino.stdTimeFunctions.isoTime,
-    transport: process.env.NODE_ENV !== "production" ?
-    {
-        target: "pino-pretty",
-        options: {
-            destination: process.stdout.fd,
-        }
-    } : undefined
+    ...transport
 });
 
+
+/* 
+
+Example child logger:
 
 export const githubLogger = logger.child({
     serviceProvider: "Github",
 })
 
-
 //Later:
-/* 
 // On success:
 log.info({
-  rows: records.length,
-  columns: records.length > 0 ? Object.keys(records[0]).length : 0,
-  success: true,
+    dataSent: {
+        rows: records.length,
+        columns: records.length > 0 ? Object.keys(records[0]).length : 0
+    },
+    success: true,
 }, 'Sync complete');
 
 // On failure:
 log.error({
-  rows: 0,
-  columns: 0,
-  success: false,
-  errorMessage: error.message,
+    dataSent: {
+        rows: 0,
+        columns: 0
+    },
+    success: false,
+    errorMessage: error.message,
 }, 'Sync failed'); 
+
 */
